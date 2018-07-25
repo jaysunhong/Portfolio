b73a260cfed2cf60943c72cdb8e6803b648c35d0
Jason Hong <JaySunHong@dhcp-v070-060.mobile.uci.edu>
2018-07-25 15:25:13 -0700

    update jul 25 2018

---
+++ assets/javascript/script.js
@@ -0,0 +1,29 @@
+$('#home-tab').on('click', function () {
+    $(this).attr('class', 'nav-link active');
+    $('#home').attr('class', 'tab-pane fade show active');
+
+    $('#resume-tab').attr('class', 'nav-link');
+    $('#resume').attr('class', 'tab-pane fade');
+    $('#portfolio-tab').attr('class', 'nav-link');
+    $('#portfolio').attr('class', 'tab-pane fade');
+});
+
+$('#resume-tab').on('click', function () {
+    $(this).attr('class', 'nav-link active');
+    $('#resume').attr('class', 'tab-pane fade show active');
+
+    $('#home-tab').attr('class', 'nav-link');
+    $('#home').attr('class', 'tab-pane fade');
+    $('#portfolio-tab').attr('class', 'nav-link');
+    $('#portfolio').attr('class', 'tab-pane fade');
+});
+
+$('#portfolio-tab').on('click', function () {
+    $(this).attr('class', 'nav-link active');
+    $('#portfolio').attr('class', 'tab-pane fade show active');
+
+    $('#home-tab').attr('class', 'nav-link');
+    $('#home').attr('class', 'tab-pane fade');
+    $('#resume-tab').attr('class', 'nav-link');
+    $('#resume').attr('class', 'tab-pane fade');
+});
